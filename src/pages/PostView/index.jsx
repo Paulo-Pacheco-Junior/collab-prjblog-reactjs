6340ff6c4aa0b5800deaca56e120d2a4716c3192
Paulo-Pacheco-Junior <paulojrgyn88@gmail.com>
2024-06-19 10:16:25 -0300

    Renderização condicional do Botão de Apagar Post no PostView para aparecer só quando o Usuário entra em um Post Criado por ele

--- src/pages/PostView/index.jsx
+++ src/pages/PostView/index.jsx
@@ -1,10 +1,13 @@
 import { Container, Post, Scroll } from "./styles";
 import { LinkBtn } from "../../components/LinkBtn";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import api from "../../services/api";
 import { useParams } from "react-router-dom";
+import { UserContext } from "../../contexts/UserContext";
 
 export function PostView() {
+  const { user } = useContext(UserContext);
+
   let { postId } = useParams();
 
   const [post, setPost] = useState({});
@@ -29,7 +32,13 @@ export function PostView() {
       <div>
         <div className="buttons-div">
           <LinkBtn to="/home" title="Voltar" />
-          <LinkBtn to="/home" onClick={handleDeletePost} title="Apagar Post" />
+          {user.id === post.user_id && (
+            <LinkBtn
+              to="/home"
+              onClick={handleDeletePost}
+              title="Apagar Post"
+            />
+          )}
         </div>
         <Post>
           <h1>{post.title}</h1>
